59bd4267c845def974bf1ab0c9ba51fc59e0e98f
Saqib Riaz <63193652+saqibriaz@users.noreply.github.com>
2020-04-21 23:15:43 -0700

    Update Main.java

--- ReadingJson/src/main/java/Main.java
+++ ReadingJson/src/main/java/Main.java
@@ -3,44 +3,45 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.ValueNode;
-
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+     public static void main(String[] args) {
         File file=new File("src/data.json");
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode jsonNode = mapper.readTree(file);
-        Map<String, String> map = new HashMap<String, String>();
-        addKeys("", jsonNode, map);
-        System.out.println(map);
+        ObjectMapper mapper=new ObjectMapper();
+        try {
 
-    }
-    private static void addKeys(String currentPath, JsonNode jsonNode, Map<String, String> map) {
-        if (jsonNode.isObject()) {
-            ObjectNode objectNode = (ObjectNode) jsonNode;
-            Iterator<Map.Entry<String, JsonNode>> iter = objectNode.fields();
-            String pathPrefix = currentPath.isEmpty()? "" : currentPath + ".";
+            LinkedHashMap<String,String> map= new LinkedHashMap<String, String>();
+            JsonNode node =mapper.readTree(file);
+            getKeys("",node, map);
 
-            while (iter.hasNext()) {
-                Map.Entry<String, JsonNode> entry = iter.next();
-                addKeys(pathPrefix + entry.getKey(), entry.getValue(), map);
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                System.out.println("Key:"+entry.getKey() + ""+"   "+" value:" + entry.getValue());
             }
-        } else if (jsonNode.isArray()) {
-            ArrayNode arrayNode = (ArrayNode) jsonNode;
-            for (int i = 0; i < arrayNode.size(); i++) {
-                addKeys(currentPath + "[" + i + "]", arrayNode.get(i), map);
-            }
-        } else if (jsonNode.isValueNode()) {
-            ValueNode valueNode = (ValueNode) jsonNode;
-            map.put(currentPath, valueNode.asText());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
-
+    public static void getKeys(String currentpath,JsonNode node,LinkedHashMap map){
+        if(node.isObject()){
+            ObjectNode objectNode=(ObjectNode) node;
+            Iterator<Map.Entry<String, JsonNode>> it=objectNode.fields();
+            String prefix=currentpath.isEmpty()?"":currentpath+".";
+            while (it.hasNext()){
+                SortedMap.Entry<String,JsonNode> iter=it.next();
+                getKeys(prefix+iter.getKey(),iter.getValue(),map);
+            }
+        }else if (node.isArray()){
+            ArrayNode arrayNode=(ArrayNode) node;
+            for(int i=0; i<arrayNode.size(); i++){
+                getKeys(currentpath+i,arrayNode.get(i),map);
+            }
+        }
+        else if(node.isValueNode()) {
+            ValueNode valueNode=(ValueNode) node;
+            map.put(currentpath,valueNode.asText());
+        }
+    }
 }
-
